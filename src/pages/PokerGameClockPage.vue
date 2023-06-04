8bb0b1ba26ae8130a64f49060a3956ffe6671cbb
Joshua Ditton <joshuaditton@internode.on.net>
2023-06-04 20:02:07 +1000

    Added a responsive image with srcset for PokerGameClockPage

--- src/pages/PokerGameClockPage.vue
+++ src/pages/PokerGameClockPage.vue
@@ -1,6 +1,19 @@
 <template>
   <q-page>
     <h1>Poker League Game Clock</h1>
+
+    <picture>
+      <img
+        sizes="(max-width: 1200px) 100vw, 1200px"
+        srcset="
+          images/game-clock/Game-Clock_start_ovdn3g_c_scale,w_400.jpg   400w,
+          images/game-clock/Game-Clock_start_ovdn3g_c_scale,w_900.jpg   900w,
+          images/game-clock/Game-Clock_start_ovdn3g_c_scale,w_1200.jpg 1200w
+        "
+        src="images/game-clock/Game-Clock_start_ovdn3g_c_scale,w_1200.jpg"
+        alt=""
+      />
+    </picture>
   </q-page>
 </template>
 
