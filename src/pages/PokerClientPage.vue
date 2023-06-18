f513e2e7eb72f2df37415a0ec4df6978427f4a2d
Joshua Ditton <joshuaditton@internode.on.net>
2023-06-18 17:12:45 +1000

    preparing a page for the Poker Thick Client Project

---
+++ src/pages/PokerClientPage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ProjectPageHeadingSection from 'src/components/ProjectPageHeadingSection.vue';
+import { pokerThickClient as project } from 'src/data/projects';
+</script>
+
+<template>
+  <q-page>
+    <ProjectPageHeadingSection :project="project" />
+  </q-page>
+</template>
