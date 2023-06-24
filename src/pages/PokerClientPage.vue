d56958385f575af7e9287451d4c3792af26ae6fc
Joshua Ditton <joshuaditton@internode.on.net>
2023-06-24 21:04:09 +1000

    begun implementing poker thick client page

--- src/pages/PokerClientPage.vue
+++ src/pages/PokerClientPage.vue
@@ -6,5 +6,71 @@ import { pokerThickClient as project } from 'src/data/projects';
 <template>
   <q-page>
     <ProjectPageHeadingSection :project="project" />
+
+    <div class="flex flex-center">
+      <div class="project-page-card">
+        <img
+          class="project-page-card-image"
+          sizes="(max-width: 531px) 100vw, 531px"
+          srcset="
+            images/poker-thick-client/load-screen/load-splash-screen_oez6ie_c_scale,w_200.png 200w,
+            images/poker-thick-client/load-screen/load-splash-screen_oez6ie_c_scale,w_399.png 399w,
+            images/poker-thick-client/load-screen/load-splash-screen_oez6ie_c_scale,w_531.png 531w
+          "
+          src="images/poker-thick-client/load-screen/load-splash-screen_oez6ie_c_scale,w_531.png"
+          alt=""
+        />
+
+        <div class="project-page-card-body">
+          <h6 class="project-page-card-heading">Loading Screen</h6>
+
+          <div class="project-page-card-description">
+            here is some description of the loading flash screen
+          </div>
+        </div>
+      </div>
+
+      <div class="project-page-card">
+        <img
+          class="project-page-card-image"
+          sizes="(max-width: 738px) 100vw, 738px"
+          srcset="
+            images/poker-thick-client/main-menu/main-screen_ylbcb1_c_scale,w_300.png 300w,
+            images/poker-thick-client/main-menu/main-screen_ylbcb1_c_scale,w_738.png 738w
+          "
+          src="images/poker-thick-client/main-menu/main-screen_ylbcb1_c_scale,w_738.png"
+          alt=""
+        />
+
+        <div class="project-page-card-body">
+          <h6 class="project-page-card-heading">Main Menu</h6>
+
+          <div class="project-page-card-description">
+            here is some description of the Main Menu
+          </div>
+        </div>
+      </div>
+
+      <div class="project-page-card">
+        <img
+          class="project-page-card-image"
+          sizes="(max-width: 751px) 100vw, 751px"
+          srcset="
+            images/poker-thick-client/sync-screen/sync-start_v3cqme_c_scale,w_300.png 300w,
+            images/poker-thick-client/sync-screen/sync-start_v3cqme_c_scale,w_751.png 751w
+          "
+          src="images/poker-thick-client/sync-screen/sync-start_v3cqme_c_scale,w_751.png"
+          alt=""
+        />
+
+        <div class="project-page-card-body">
+          <h6 class="project-page-card-heading">Synchronisation</h6>
+
+          <div class="project-page-card-description">
+            here is some description of the Synchronisation screen
+          </div>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
